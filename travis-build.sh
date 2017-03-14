bc5e4609e3ae0e636e7944ed4326a1e46dbab386
Ian Craggs <icraggs@gmail.com>
2017-03-14 21:27:24 +0000

    Environment variables

--- travis-build.sh
+++ travis-build.sh
@@ -5,6 +5,7 @@ set -e
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
   mkdir build.paho
   cd build.paho
+  echo "travis build dir $TRAVIS_BUILD_DIR pwd $PWD"
   cmake -DPAHO_WITH_SSL=TRUE -DPAHO_BUILD_DOCUMENTATION=FALSE -DPAHO_BUILD_SAMPLES=TRUE ..
   make
   ctest -VV
@@ -13,6 +14,7 @@ fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   mkdir build.paho
   cd build.paho
+  echo "travis build dir $TRAVIS_BUILD_DIR pwd $PWD"
   cmake -DPAHO_WITH_SSL=TRUE -DPAHO_BUILD_DOCUMENTATION=FALSE -DPAHO_BUILD_SAMPLES=TRUE ..
   make
   ctest -VV
