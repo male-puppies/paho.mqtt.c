3da32b8477c7fa2e0cdeab012288d8bac028eabe
Ian Craggs <icraggs@uk.ibm.com>
2017-03-15 09:16:54 +0000

    Start mosquitto

--- travis-install.sh
+++ travis-install.sh
@@ -3,12 +3,12 @@
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 	pwd
 	mosquitto -h
-	mosquitto -c test/tls-testing/mosquitto.conf \&
+	mosquitto -c test/tls-testing/mosquitto.conf &
 fi
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 	pwd
 	brew update
 	brew install openssl mosquitto
-	brew services start mosquitto -c test/tls-testing/mosquitto.conf
+	mosquitto -c test/tls-testing/mosquitto.conf &
 fi
